d47773b41bc0a054138c295043f04744dcb52fe4
robert.geica <geicarobert@gmail.com>
2025-08-02 15:29:02 +0300

    feat(server): add stop script path, update .env.example

--- server/constants.js
+++ server/constants.js
@@ -4,3 +4,4 @@ dotenv.config();
 export const remoteUser = process.env.SSH_USER;
 export const remoteHost = process.env.SSH_HOST;
 export const remoteScriptPath = process.env.SCRIPT_PATH;
+export const remoteStopScriptPath = process.env.STOP_SCRIPT_PATH;
